f94d294158a8b95a50d5246c4ede687de104c511
Ignacio Pérez <ignacio@refy.cl>
2022-08-25 16:42:05 -0400

    adding entrypoint

---
+++ app/docker/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+TRY_LOOP="20"
+
+: "${POSTGRES_HOST:="postgres"}"
+: "${POSTGRES_PORT:="5433"}"
+
+wait_for_port() {
+  local name="$1" host="$2" port="$3"
+  local j=0
+  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
+    j=$((j+1))
+    if [ $j -ge $TRY_LOOP ]; then
+      echo >&2 "$(date) - $host:$port still not reachable, giving up"
+      exit 1
+    fi
+    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
+    sleep 5
+  done
+}
+
+echo "$POSTGRES_HOST" "$POSTGRES_PORT"
+wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
+
+echo "starting refy api"
+
+uvicorn main:app --port=$SERVICE_PORT --host=0.0.0.0
